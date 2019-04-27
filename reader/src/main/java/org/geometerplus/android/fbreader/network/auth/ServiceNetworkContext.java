d2c98498e03bd51bc61d93f1f52c312668c0328f
Khaleel Jageer <khaleel@theviralfever.com>
2019-04-27 23:14:43 +0530

    icon added

--- reader/src/main/java/org/geometerplus/android/fbreader/network/auth/ServiceNetworkContext.java
+++ reader/src/main/java/org/geometerplus/android/fbreader/network/auth/ServiceNetworkContext.java
@@ -19,54 +19,54 @@
 
 package org.geometerplus.android.fbreader.network.auth;
 
-import java.net.URI;
-import java.util.Map;
-
-import android.app.*;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-
 import androidx.core.app.NotificationCompat;
 import org.geometerplus.zlibrary.core.resources.ZLResource;
-
 import org.geometerplus.zlibrary.ui.android.R;
 
+import java.net.URI;
+import java.util.Map;
+
 public class ServiceNetworkContext extends AndroidNetworkContext {
-	private final Service myService;
+    private final Service myService;
 
-	public ServiceNetworkContext(Service service) {
-		myService = service;
-	}
+    public ServiceNetworkContext(Service service) {
+        myService = service;
+    }
 
-	public Context getContext() {
-		return myService;
-	}
+    public Context getContext() {
+        return myService;
+    }
 
-	@Override
-	protected Map<String,String> authenticateWeb(URI uri, String realm, String authUrl, String completeUrl, String verificationUrl) {
-		final NotificationManager notificationManager =
-			(NotificationManager)myService.getSystemService(Context.NOTIFICATION_SERVICE);
-		final Intent intent = new Intent(myService, WebAuthorisationScreen.class);
-		intent.setData(Uri.parse(authUrl));
-		intent.putExtra(WebAuthorisationScreen.COMPLETE_URL_KEY, completeUrl);
-		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		intent.addFlags(Intent.FLAG_FROM_BACKGROUND);
-		final PendingIntent pendingIntent = PendingIntent.getActivity(myService, 0, intent, 0);
-		final String text =
-			ZLResource.resource("dialog")
-				.getResource("backgroundAuthentication")
-				.getResource("message")
-				.getValue();
-		final Notification notification = new NotificationCompat.Builder(myService)
-			.setSmallIcon(R.drawable.fbreader)
-			.setTicker(text)
-			.setContentTitle(realm)
-			.setContentText(text)
-			.setContentIntent(pendingIntent)
-			.setAutoCancel(true)
-			.build();
-		notificationManager.notify(0, notification);
-		return errorMap("Notification sent");
-	}
+    @Override
+    protected Map<String, String> authenticateWeb(URI uri, String realm, String authUrl, String completeUrl, String verificationUrl) {
+        final NotificationManager notificationManager =
+                (NotificationManager) myService.getSystemService(Context.NOTIFICATION_SERVICE);
+        final Intent intent = new Intent(myService, WebAuthorisationScreen.class);
+        intent.setData(Uri.parse(authUrl));
+        intent.putExtra(WebAuthorisationScreen.COMPLETE_URL_KEY, completeUrl);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_FROM_BACKGROUND);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(myService, 0, intent, 0);
+        final String text = ZLResource.resource("dialog")
+                .getResource("backgroundAuthentication")
+                .getResource("message")
+                .getValue();
+        final Notification notification = new NotificationCompat.Builder(myService)
+                .setSmallIcon(R.drawable.fbreader)
+                .setTicker(text)
+                .setContentTitle(realm)
+                .setContentText(text)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build();
+        notificationManager.notify(0, notification);
+        return errorMap("Notification sent");
+    }
 }
