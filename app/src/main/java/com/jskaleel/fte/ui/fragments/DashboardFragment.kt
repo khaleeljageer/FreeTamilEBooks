98f2a1c81eb48ced2eee1026b7f8fa9c21b25196
Khaleel Jageer <jskcse4@gmail.com>
2021-02-13 21:27:17 +0530

    dashboard page added

---
+++ app/src/main/java/com/jskaleel/fte/ui/fragments/DashboardFragment.kt
@@ -0,0 +1,20 @@
+package com.jskaleel.fte.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jskaleel.fte.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
